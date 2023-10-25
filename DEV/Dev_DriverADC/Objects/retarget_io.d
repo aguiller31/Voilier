7b53026d1535a62167e10f3b307f14acf74ea0a1
aguiller31 <antoine.guillermin31@gmail.com>
2023-10-25 17:59:51 +0200

    Added .gitignore

--- DEV/Dev_DriverADC/Objects/retarget_io.d
+++
@@ -1,8 +0,0 @@
-.\objects\retarget_io.o: D:/Users/antoi/AppData/Local/Arm/Packs/Keil/ARM_Compiler/1.7.2/Source/retarget_io.c
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\string.h
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\assert.h
-.\objects\retarget_io.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\rt_sys.h
-.\objects\retarget_io.o: .\RTE\_Reel\RTE_Components.h
