eef8d8d67b44c477ea5ecda16ca9872324411f78
paul-gadanho <150153491+paul-gadanho@users.noreply.github.com>
2023-11-21 11:08:18 +0100

    Update accelero.c

--- Services/Src/accelero.c
+++ Services/Src/accelero.c
@@ -4,7 +4,8 @@
 
 #define first_bit 0x80 // Je veux lire 
 #define BW_RATE_W 0x2C
-#define BW_RATE_R 0xAC
+// #define BW_RATE_R 0xAC
+char BW_RATE_R = first_bit | BW_RATE_W;
 
 #define POWER_CTL_W 0x2D
 #define POWER_CTL_R 0xAD
@@ -19,24 +20,29 @@ void Accelero_Init (SPI_TypeDef * SPI) {
 	
 	  MySPI_Init(SPI);
 	
-		MySPI_Send(BW_RATE_R);	// Je veux lit BW_RATE
-		tps = MySPI_Read();			// Je lit BW_RATE
-		tps  &= 0; 							// reset Bw_Rate
-		tps |= 0x0A; 						// BW_rate  à 100Hz
-		MySPI_Send(BW_RATE_W);	// Je veux écrire BW_RATE
-		MySPI_Send(tps);				// J'écrit BW_RATE
-	
+
+		tps = 0x0A; 								// BW_rate  Ã  100Hz
+		MySPI_Set_NSS();						// CS Ã  0
+		MySPI_Send(BW_RATE_W);			// Je veux Ã©crire BW_RATE
+		MySPI_Send(tps);						// J'Ã©crit BW_RATE
+
 		MySPI_Send(POWER_CTL_R);		// Je veux lit POWER_CTL
 		tps = MySPI_Read();					// Je lit POWER_CTL
+		MySPI_Clear_NSS();					// CS Ã  1	
 		tps  |= 1<<3; 							// measure de power ctrl = 1
-		MySPI_Send(POWER_CTL_W);		// Je veux écrire POWER_CTL
-		MySPI_Send(tps);						// J'écrit POWER_CTL
+		MySPI_Set_NSS();						// CS Ã  0	
+		MySPI_Send(POWER_CTL_W);		// Je veux Ã©crire POWER_CTL
+		MySPI_Send(tps);						// J'Ã©crit POWER_CTL
+
 	
 		MySPI_Send(DATA_FORMAT_R);	// Je veux lit DATA_FORMAT
 		tps = MySPI_Read();					// Je lit POWER_CTL
+		MySPI_Clear_NSS();					// CS Ã  1	
 		tps  &= ~(1<<2); 						// Justify de Data Format = 0
-		tps	|= 0x0B; 								// Full_reso = 1 et Range bit = 11 (+- 16g) 
-		MySPI_Send(DATA_FORMAT_W);	// Je veux écrire DATA_FORMAT
-		MySPI_Send(tps);						// J'écrit POWER_CTL
+		tps	|= 0x0B; 								// Full_reso = 1 et Range bit = 11 (+- 16g)
+		MySPI_Set_NSS();						// CS Ã  0
+		MySPI_Send(DATA_FORMAT_W);	// Je veux Ã©crire DATA_FORMAT
+		MySPI_Send(tps);						// J'Ã©crit POWER_CTL
+		MySPI_Clear_NSS();					// CS Ã  1	
 		 
 }
